ffb8334d3f6ade19404dd305da047b04dd78ad1b
iss12 <Ivson_Santos@epam.com>
2022-09-16 17:59:01 +0200

    Projeto criado

---
+++ src/main/java/com/ivson/springecommerce/model/Product.java
@@ -0,0 +1,50 @@
+package com.ivson.springecommerce.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private LocalDate dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private LocalDate lastUpdated;
+
+}
