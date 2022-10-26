0ffb734fa9b2a9c207777ff0dfa7a532f7c706e4
iss12 <Ivson_Santos@epam.com>
2022-10-26 20:29:41 +0200

    Cross origin

--- src/main/java/com/ivson/springecommerce/repository/ProductCategoryRepository.java
+++ src/main/java/com/ivson/springecommerce/repository/ProductCategoryRepository.java
@@ -4,8 +4,10 @@ import com.ivson.springecommerce.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // name of JSON referencey, path to the resource
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
